0545c5aa115ec310014c7b7b7cb4bcf8745e9593
igrika <111415524+igrika@users.noreply.github.com>
2025-01-15 11:14:20 +0400

    Update Back elements

---
+++ Assets/Match_Color_Frogs/Runtime/MatchColorFrogsEntryPoint.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using com.appidea.MiniGamePlatform.CommunicationAPI;
+using Managers;
+using UnityEngine;
+
+public class MatchColorFrogsEntryPoint : BaseMiniGameEntryPoint
+{
+    [SerializeField] private GameObject gamePrefab;
+    protected override Task LoadInternal()
+    {
+        var gameManager = Instantiate(gamePrefab);
+        gameManager.GetComponent<GameManager>().SetEntryPoint(this);
+        return Task.CompletedTask;
+    }
+
+    protected override Task UnloadInternal()
+    {
+        return Task.CompletedTask;
+    }
+
+
+}
